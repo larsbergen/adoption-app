6017fbbe28c6ea3fe37ba37130235ca81846c5f1
LBergen <lars.derek.bergen@gmail.com>
2018-09-07 12:33:48 -0700

    working routes

---
+++ src/components/NavBar.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './NavBar.css';
+import { Link } from 'react-router-dom';
+class NavBar extends Component {
+    constructor(props) {
+    super(props);
+
+
+    this.state = {
+        currentlySelectedTab: 0,
+    }
+    this.navBarConfig=[{name: 'Home', link:'/'},{name: 'Saved Pets', link:'/savedpets'}];
+    }
+
+
+    render() {
+        return (
+            <div className='navbar-container'>
+                <div className="navbar-links">
+                    {this.navBarConfig.map((navBarItem, index)=> {
+                       return (
+                       <div className="nav-link-holder"><Link to={navBarItem.link}><span onClick={()=>{this.navBarItem}} className={`anchor ${index === this.state.currentlySelectedTab ? 'selected-tab' : null}`}>{navBarItem.name}</span></Link></div>
+                    );
+                    })}
+                </div>
+            </div>
+        );
+    }
+}
+export default NavBar;
