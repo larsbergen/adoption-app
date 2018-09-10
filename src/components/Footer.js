1c070cf7b8f5fa336bb958d92bdd4c600ac9ff0a
Ty Augusta <taugusta06@gmail.com>
2018-09-10 10:51:15 -0700

    footer css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './Footer.css';
-
+var Facebook = require('./images/faceBook-blue.png');
+var Twitter = require('./images/tw-bl-sm.png');
 class Footer extends Component {
     render() {
         return (
             <div className='footer-container'>
             <div className='social-media-links'>
-            <a>hi</a>
-            <span> | </span>
-            <a>hi</a>
+            <img src={Facebook} className='socialmedia-fb' alt='Facebok'></img>
+            <img src={Twitter} className='socialmedia-tw' alt='Twitter'></img>
             </div>
             </div>
         );
